a00c574c661f06453eb46f6108ae653125d413ea
Preeti Kant <44994724+preetikant@users.noreply.github.com>
2019-11-04 15:31:27 +0530

    Add files via upload

---
+++ wrapper_class/Test_Boolean.java
@@ -0,0 +1,12 @@
+class Test_Boolean{
+	public static void main(String args[]){
+	
+		Boolean x = new Boolean("Yes");
+		Boolean y = new Boolean("no");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(x.equals(y));
+				
+		
+	}
+}
